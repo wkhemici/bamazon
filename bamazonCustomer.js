de1be7f2908acac2731e6f5c53b468b29956543e
wkhemici <47515500+wkhemici@users.noreply.github.com>
2019-04-02 13:36:58 -0700

    Update bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
     host: "localhost",
 
-    // Your port; if not 3306
+    // Your port;
     port: 3306,
 
     // Your username
@@ -18,7 +18,7 @@ connection.connect(function (err, res) {
     if (err) throw err;
     queryProducts();
     console.log("Showing you our products...")
-    console.log(res);
+    // console.log(res);
     // searchProducts();
     // searchQuanitity()
 })
@@ -52,45 +52,49 @@ function queryProducts() {
             },
             // Here we give the user a list to choose from.
 
-    ])
-    .then(function (inquirerResponse) {
-           
-        connection.query("SELECT stock_quantity FROM products WHERE ?", { product_name: inquirerResponse.products }, function (err, res) {
-            if (err) throw err;
-            for (var i = 0; i < res.length; i++) {
-            if (res[i].stock_quantity < inquirerResponse.quantity) {
-                console.log("We only have " + res[i].stock_quantity + " in stock. Please select fewer items and start over.")
-        } else { 
-        connection.query("SELECT price FROM products WHERE ?", { product_name: inquirerResponse.products }, function (err, res) {
-            if (err) throw err;
-            for (var i = 0; i < res.length; i++) {
-                // if (res[i].stock_quantity > inquirerResponse.quantity) {
-                console.log("You purchased " + inquirerResponse.quantity + " " + inquirerResponse.products + " items.");
-                console.log("The total price for your purchase is $" + res[i].price * inquirerResponse.quantity)
-                // };
-     }
+        ])
+        .then(function (inquirerResponse) {
 
-     connection.end();
- }
-    )
-            };
+            connection.query("SELECT stock_quantity FROM products WHERE ?", { product_name: inquirerResponse.products }, function (err, res) {
+                if (err) throw err;
+                for (var i = 0; i < res.length; i++) {
+                    if (res[i].stock_quantity < inquirerResponse.quantity) {
+                        console.log("We only have " + res[i].stock_quantity + " in stock. Please select fewer items and start over.")
+                    } else {
+                        connection.query("SELECT price, stock_quantity FROM products WHERE ?", { product_name: inquirerResponse.products }, function (err, res) {
+                            if (err) throw err;
+                            for (var i = 0; i < res.length; i++) {
+                                // if (res[i].stock_quantity > inquirerResponse.quantity) {
+                                console.log("You purchased " + inquirerResponse.quantity + " " + inquirerResponse.products + " items.");
+                                console.log("The total price for your purchase is $" + res[i].price * inquirerResponse.quantity)
+                                // };
+                                
+                                var userInput = parseInt(inquirerResponse.quantity)
+                                var dataQuantity = res[i].stock_quantity - userInput;
+                                console.log(res[i].stock_quantity);
+                                // console.log(dataQuantity);
+                                console.log(typeof (dataQuantity))
+                                var query = "UPDATE products SET ? WHERE ?;"
+                                 connection.query(query,
+                                    [
+                                        {
+                                            stock_quantity: dataQuantity
+                                        },
+                                        { product_name: inquirerResponse.products }
 
-        }
-        });
-})
+                                    ],
+                                )
+                                // console.log(res[i].stock_quantity);
+                            }
 
-     }
 
-//My attempt at updated mySQL:
-//      let sql = `UPDATE products,
-//            SET stock_quantity = ?,
-//            WHERE  = ?`;
- 
-// let data = [false, 1];
-// connection.query(sql, data, (error, results, fields) => {
-//     if (error){
-//       return console.error(error.message);
-//     }
-//     console.log('Rows affected:', results.affectedRows);
-//   });
+                            connection.end();
+                        }
+                        )
+                    };
 
+                }
+            });
+        })
+
+}
